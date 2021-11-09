d5ad8cf09fc381cbc6143684e831bf05a13c6134
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2021-11-09 09:22:51 +0530

    first

---
+++ Basics/Object.py
@@ -0,0 +1,11 @@
+e_id =101
+e_name ="Ravi"
+rating = 4.3
+size = [38,40,42]
+size1 = (38,40,42)
+size2 = {38,40,42}
+emp = {'name':"Ravi", 'sal':45000}
+x = range(0,10)
+discount = True
+
+print(type(discount))
---
+++ Basics/new.py
@@ -0,0 +1,8 @@
+size = [38,40,42,44]
+b = bytearray(size)
+
+print(type(b))
+
+size1 = {38,40,40}
+fs=frozenset(size1)
+print(type(fs))
